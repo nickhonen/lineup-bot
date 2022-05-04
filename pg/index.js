94719652ea366a40542ccc5be845441a4aec6c88
Nicholas Honen <65134752+nickhonen@users.noreply.github.com>
2022-05-04 12:31:19 -0400

    can query db from slash command

--- pg/index.js
+++ pg/index.js
@@ -1,4 +1,4 @@
-const { Pool } = require('pg');
+const { Pool, Client } = require('pg');
 
 // works without connection information bc it uses env variables
 const pool = new Pool({
@@ -17,13 +17,44 @@ const pool = new Pool({
 // })
 
 // test query
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  pool.end()
-})
 
-// From "Express with async/await" part of guide.
+// Copy pasted directly from suggested project structure, probably don't need all of this.
 module.exports = {
-  query: (text, params) => pool.query(text, params),
+  async query(text, params) {
+    const start = Date.now()
+    const res = await pool.query(text, params)
+    const duration = Date.now() - start
+    console.log('executed query', { text, duration, rows: res.rowCount })
+    return res
+  },
+
+  async getClient() { 
+    const client = await pool.connect()
+    const query = client.query
+    const release = client.release
+    // set a timeout of 5 seconds, after which we will log this client's last query
+    const timeout = setTimeout(() => {
+      console.error('A client has been checked out for more than 5 seconds!')
+      console.error(`The last executed query on this client was: ${client.lastQuery}`)
+    }, 5000)
+    // monkey patch the query method to keep track of the last query executed
+    client.query = (...args) => {
+      client.lastQuery = args
+      return query.apply(client, args)
+    }
+    client.release = () => {
+      // clear our timeout
+      clearTimeout(timeout)
+      // set the methods back to their old un-monkey-patched version
+      client.query = query
+      client.release = release
+      return release.apply(client)
+    }
+    return client
+  },
+
+  // added for quick tests, add async compatibility
+  simpleQuery: (text, params) => pool.query(text, params),
+
 }
 
